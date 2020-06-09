3bcc7c1ef953f87236d24b62c6b43d54a0344250
zjianfa <zjianfa@foxmail.com>
2020-06-09 23:27:06 +0800

    unzip

---
+++ 前端仓库/ABS_Fabric_Antdesignpro/src/mock/services/manage.js
@@ -0,0 +1,127 @@
+import Mock from 'mockjs2'
+import { builder, getQueryParameters } from '../util'
+
+const totalCount = 5701
+
+const serverList = (options) => {
+  const parameters = getQueryParameters(options)
+
+  const result = []
+  const pageNo = parseInt(parameters.pageNo)
+  const pageSize = parseInt(parameters.pageSize)
+  const totalPage = Math.ceil(totalCount / pageSize)
+  const key = (pageNo - 1) * pageSize
+  const next = (pageNo >= totalPage ? (totalCount % pageSize) : pageSize) + 1
+
+  for (let i = 1; i < next; i++) {
+    const tmpKey = key + i
+    result.push({
+      key: tmpKey,
+      id: tmpKey,
+      no: 'No ' + tmpKey,
+      description: '这是一段描述',
+      callNo: Mock.mock('@integer(1, 999)'),
+      status: Mock.mock('@integer(0, 3)'),
+      updatedAt: Mock.mock('@datetime'),
+      editable: false
+    })
+  }
+
+  return builder({
+    pageSize: pageSize,
+    pageNo: pageNo,
+    totalCount: totalCount,
+    totalPage: totalPage,
+    data: result
+  })
+}
+
+const projects = () => {
+  return builder({
+    'data': [{
+      id: 1,
+      cover: 'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png',
+      title: '交易主界面',
+      description: '正在开发中......',
+      status: 1,
+      updatedAt: '2020-05 00:00:00'
+    }
+    ],
+    'pageSize': 10,
+    'pageNo': 0,
+    'totalPage': 6,
+    'totalCount': 57
+  })
+}
+
+const activity = () => {
+  return builder([{
+    id: 1,
+    user: {
+      nickname: '@name',
+      avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png'
+    },
+    project: {
+      name: 'SCU-ABS-Fabric',
+      action: '更新了',
+      event: '开发计划'
+    },
+    time: '2020-05 14:00:00'
+  }
+  ])
+}
+
+const teams = () => {
+  return builder([{
+    id: 1,
+    name: 'SCU-ABS-Fabric',
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png'
+  }
+  ])
+}
+
+const radar = () => {
+  return builder([{
+    item: '引用',
+    '个人': 70,
+    '团队': 30,
+    '部门': 40
+  },
+  {
+    item: '口碑',
+    '个人': 60,
+    '团队': 70,
+    '部门': 40
+  },
+  {
+    item: '产量',
+    '个人': 50,
+    '团队': 60,
+    '部门': 40
+  },
+  {
+    item: '贡献',
+    '个人': 40,
+    '团队': 50,
+    '部门': 40
+  },
+  {
+    item: '热度',
+    '个人': 60,
+    '团队': 70,
+    '部门': 40
+  },
+  {
+    item: '引用',
+    '个人': 70,
+    '团队': 50,
+    '部门': 40
+  }
+  ])
+}
+
+Mock.mock(/\/service/, 'get', serverList)
+Mock.mock(/\/list\/search\/projects/, 'get', projects)
+Mock.mock(/\/workplace\/activity/, 'get', activity)
+Mock.mock(/\/workplace\/teams/, 'get', teams)
+Mock.mock(/\/workplace\/radar/, 'get', radar)
